7b58b2b31eb88c4c51aabdb4212a39eeb1cca2b9
Scott Tran <transt@gmail.com>
2015-03-13 13:20:08 -0500

    Rename IBM config file, aix.py config route to en0

--- cloudinit/config/cc_update_bootlist.py
+++ cloudinit/config/cc_update_bootlist.py
@@ -1,10 +1,18 @@
 # =================================================================
-# Licensed Materials - Property of IBM
 #
-# (c) Copyright IBM Corp. 2015 All Rights Reserved
+#    (c) Copyright IBM Corp. 2015 
 #
-# US Government Users Restricted Rights - Use, duplication or
-# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # =================================================================
 
 import re
